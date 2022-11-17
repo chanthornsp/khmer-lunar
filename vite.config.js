b94c15454e4b3a61852b5c275c8a90a39eaa7451
ChanthornAcademy <hoeurnchanthorn@gmail.com>
2022-11-17 10:59:30 +0700

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/khmer-lunar/',
     plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
     build: {
         chunkSizeWarningLimit: 2000,
     },
