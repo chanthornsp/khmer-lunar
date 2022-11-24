f693730bd3a7caa220cc8711334838f22ebae951
chanthornacademy <hoeurnchanthorn@gmail.com>
2022-11-24 15:55:59 +0700

    fixed some performance

--- src/Pages/KhmerLurnaTable.vue
+++ src/Pages/KhmerLurnaTable.vue
@@ -1,18 +1,43 @@
 <script setup>
 import useKhmerDate from "../Composables/useKhmerDate.js";
 import moment from "moment/min/moment-with-locales";
+import { debounce } from "lodash";
 import { onMounted, ref, watch } from "vue";
 
 const { khmerDate } = useKhmerDate();
 const currentYear = ref(moment().year());
 const yearList = ref([]);
 const dates = ref([]);
-onMounted(() => {
-  //  init full year dates
-  init();
+const loaded = ref(false);
+onMounted(
+  debounce(() => {
+    //  init full year dates
+    init().then(() => {
+      loaded.value = true;
+    });
+    for (let x = 1900; x <= 2900; x++) {
+      yearList.value.push(x);
+    }
+  }, 200)
+);
+const filterForm = ref({
+  year: currentYear.value,
 });
-const init = () => {
-  yearList.value.length = 0;
+watch(
+  filterForm.value,
+  debounce(() => {
+    currentYear.value = filterForm.value.year;
+    init().then(() => {
+      loaded.value = true;
+    });
+  }, 200)
+);
+
+watch(filterForm.value, () => {
+  loaded.value = false;
+});
+
+const init = async () => {
   dates.value.length = 0;
   for (let month = 0; month < 12; month++) {
     let daysInMonth = moment({
@@ -27,16 +52,20 @@ const init = () => {
         M: month,
         d: i,
       });
-      let khmerLurnaDate = khmerDate.value(currentDate);
+      // let khmerLurnaDate = khmerDate.value(currentDate);
+      let khmerLurnaDate = {
+        khDay: khmerDate.value(currentDate).toKhDate("dN"),
+        khMonth: khmerDate.value(currentDate).toKhDate("m"),
+        animalYear: khmerDate.value(currentDate).toKhDate("a"),
+        khYear: khmerDate.value(currentDate).toKhDate("e"),
+        beYear: getBEYear(currentDate),
+      };
       dates.value.push({
         day: i,
         date: currentDate,
         khDate: khmerLurnaDate,
       });
     }
-  }
-  for (let x = 1900; x <= 2900; x++) {
-    yearList.value.push(x);
   }
 };
 
@@ -47,14 +76,6 @@ const getBEYear = (date) => {
     return date.clone().add(544, "years").locale("km").format("YYYY");
   }
 };
-const filterForm = ref({
-  year: currentYear.value,
-});
-
-watch(filterForm.value, () => {
-  currentYear.value = filterForm.value.year;
-  init();
-});
 </script>
 
 <template>
@@ -71,6 +92,7 @@ watch(filterForm.value, () => {
       >
         <thead>
           <tr>
+            <th>Nº</th>
             <th>Day</th>
             <th>Khmer Day</th>
             <th>Month</th>
@@ -82,16 +104,33 @@ watch(filterForm.value, () => {
           </tr>
         </thead>
         <tbody>
-          <template v-for="(date, index) in dates" :key="index">
-            <tr :class="date.day === 1 ? 'row-success' : ''">
-              <td>{{ date.day }}</td>
-              <td>{{ date.khDate.toKhDate("dN") }}</td>
-              <td>{{ date.date.format("MMMM") }}</td>
-              <td>{{ date.khDate.toKhDate("m") }}</td>
-              <td>{{ date.date.format("YYYY") }}</td>
-              <td>{{ getBEYear(date.date) }}</td>
-              <td>{{ date.khDate.toKhDate("a") }}</td>
-              <td>{{ date.khDate.toKhDate("e") }}</td>
+          <template v-if="loaded">
+            <template v-for="(date, index) in dates" :key="index">
+              <tr :class="date.day === 1 ? 'row-success' : ''">
+                <td>{{ index + 1 }}</td>
+                <td>{{ date.day }}</td>
+                <td>{{ date.khDate.khDay }}</td>
+                <td>{{ date.date.format("MMMM") }}</td>
+                <td>{{ date.khDate.khMonth }}</td>
+                <td>{{ date.date.format("YYYY") }}</td>
+                <td>{{ date.khDate.beYear }}</td>
+                <td>{{ date.khDate.animalYear }}</td>
+                <td>{{ date.khDate.khYear }}</td>
+              </tr>
+            </template>
+          </template>
+          <template v-else>
+            <tr>
+              <td colspan="9" class="!bg-gray-500 animate-pulse">Loading...</td>
+            </tr>
+            <tr>
+              <td colspan="9" class="!bg-gray-500 animate-pulse">Loading...</td>
+            </tr>
+            <tr>
+              <td colspan="9" class="!bg-gray-500 animate-pulse">Loading...</td>
+            </tr>
+            <tr>
+              <td colspan="9" class="!bg-gray-500 animate-pulse">Loading...</td>
             </tr>
           </template>
         </tbody>
